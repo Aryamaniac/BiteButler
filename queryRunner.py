77f547227b547f40a2ed0567e3bc7249ada3c02a
Mitchell Casleton <54728519+MitchellCasleton@users.noreply.github.com>
2023-02-04 15:18:06 -0500

    Updated queryRunner to work with writing data to firebase

--- queryRunner.py
+++ queryRunner.py
@@ -5,42 +5,8 @@ from firebase_admin import firestore
 from firebase import Firebase
 
 cred = credentials.Certificate("serviceAccountKey.json")
-#firebase_admin.initialize_app(cred, {'databaseURL': 'https://bitebutler-e193b-default-rtdb.firebaseio.com/'}) https://bitebutler-e193b.firebaseapp.com/
-app = firebase_admin.initialize_app(cred)
-firestore_client = firestore.client()
-doc_ref = firestore_client.collection('locations')
-print(f"tehdcoucment id is {doc_ref.id}")
-# doc = doc_ref.get()
-# print(f"The docutment is {doc.to_dict()}")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://bitebutler-e193b-default-rtdb.firebaseio.com/'})
 
-# ref = db.reference("/locations")
-# print(ref.get())
-
-#ref = db.reference("path/to/data")
-#data = ref.get()
-
-# data = db.collection(u"locations")
-# docs = data.stream()
-
-# for doc in docs:
-#     print(f'{doc.id} => {doc.to_dict()}')
-
-# print(data)
-
-# config = {
-#     "apiKey":  "AIzaSyAHQhCz26Et5hv21rekZrPc2mX8HQ4rYl0",
-
-#     "authDomain": "bitebutler-e193b.firebaseapp.com",
-#     "databaseURL": "https://bitebutler-e193b.web.app",
-
-#   "storageBucket": "bitebutler-e193b.appspot.com",
-
-# }
-
-# firebase = Firebase(config)
-# db = firebase.database()
-# locations = db.child('locations').get()
-# print(locations.val())
 
 class queryRunner:
     def __init__(self):
@@ -50,8 +16,9 @@ class queryRunner:
         self.yelpRestaurants = None
 
     def get_input(self):
-        self.city = input("Enter city: ")
-        self.state = input("Enter state: ")
+        ref = db.reference("/locations")
+        self.city = ref.get()['city']
+        self.state = ref.get()['state']
 
     def run_yelp_query(self):
         from yelpFetcher import YelpRestaurantFetcher
@@ -67,6 +34,10 @@ class queryRunner:
                 restaurant["score"] += past_choices[restaurant["name"]] * choice_weight
         sorted_restaurants = sorted(restaurants, key=lambda x: x["score"], reverse=True)
         return sorted_restaurants
+    
+    def write_restaurants(self, restaurants):
+        ref = db.reference("/restaurants")
+        ref.set(restaurants)
         
     def main(self):
         """takes input from user, runs yelp query, sorts restaurants, and prints results
@@ -76,6 +47,8 @@ class queryRunner:
         sortedRestaurants = self.sort_restaurants(yelpRestaurants, {})
         for restaurant in sortedRestaurants:
             print(restaurant["name"], restaurant["score"])
+        self.write_restaurants(sortedRestaurants)
+        print("done")
 
 temp = queryRunner()
 temp.main()
